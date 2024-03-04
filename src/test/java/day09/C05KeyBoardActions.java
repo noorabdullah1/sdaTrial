f34c221691c16572489b3f8a158d823594c8d35d
noorabdullah1 <noor-babahr@hotmail.com>
2024-03-05 00:48:33 +0300

    5/3

---
+++ src/test/java/day09/C05KeyBoardActions.java
@@ -0,0 +1,14 @@
+//package day09;
+//
+//import org.junit.Test;
+//import utilities.TestBase;
+//
+//public class C05KeyBoardActions extends TestBase {
+////    Go to URL: https://demoqa.com/auto-complete
+////    In the Type single color name section, print "Exceptional" by using the action methods.
+//
+//  @Test
+//  public void kk {
+//        driver.get("https://demoqa.com/auto-complete");
+//    }
+//}
